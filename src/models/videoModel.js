f36915e296550411c2294de07504f9fef23fe0fa
SyedMuhgisHussain <syedmughis4541@gmail.com>
2024-03-07 20:09:37 +0500

    Video model added

--- src/models/videoModel.js
+++ src/models/videoModel.js
@@ -1,10 +1,45 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const videoSchema = new Schema({
-    
-})
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String,
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const Video =  mongoose.model('Video', videoSchema);
+const Video = mongoose.model("Video", videoSchema);
 
 export default Video;
-
